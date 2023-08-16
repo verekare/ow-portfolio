985403a9747ae33cad3afaedf5a3886fd6351cfc
Nastia <snasfibz05@gmail.com>
2023-08-16 18:57:11 +0300

    Initial commit

---
+++ src/js/accordion.js
@@ -0,0 +1,15 @@
+    // OPEN/CLOSE ACCORDION
+    let acc = document.getElementsByClassName("accordion");
+    for (let i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        let panel = this.nextElementSibling;
+      
+                // toggle between hide / show
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+    
+      });
+      }
---
+++ src/js/tabs.js
@@ -0,0 +1,25 @@
+    // SWITCHING THE TABS
+            // (tab-content, clicked tab-link)
+function openTab(tabId, elem) { 
+            // tab-link collection
+  let tabLinks = Array.from(document.querySelector('.tabs-nav').children);
+
+            // make all tabs thin
+  for (let tab of tabLinks) {
+    tab.style.flex = 0;
+  }
+
+            // except the one that was clicked
+  elem.style.flex = 1;
+
+            // find all content
+  let tabContents = document.querySelectorAll('.tab-content');
+  
+            // hide everything
+  for (let content of tabContents) {
+    content.style.display = 'none';
+  }
+
+            // except chosen content
+  document.getElementById(tabId).style.display = 'block';
+}
