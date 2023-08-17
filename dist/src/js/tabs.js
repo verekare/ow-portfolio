19277e2e92732b82ce834c372490c41f76e9ba5b
Nastia <snasfibz05@gmail.com>
2023-08-17 19:19:05 +0300

    Tabs toggling

--- dist/src/js/tabs.js
+++ dist/src/js/tabs.js
@@ -1,25 +1,38 @@
     // SWITCHING THE TABS
             // (tab-content, clicked tab-link)
-function openTab(tabId, elem) { 
-            // tab-link collection
-  let tabLinks = Array.from(document.querySelector('.tabs-nav').children);
 
-            // make all tabs thin
+  // tab-link array
+let tabLinks = Array.from(document.querySelectorAll(".tab-link"));
+  // find all content
+let tabContents = document.querySelectorAll('.tab-content');
+
+function closeTabs() { 
+
   for (let tab of tabLinks) {
     tab.style.flex = 0;
   }
 
-            // except the one that was clicked
-  elem.style.flex = 1;
-
-            // find all content
-  let tabContents = document.querySelectorAll('.tab-content');
-  
-            // hide everything
   for (let content of tabContents) {
     content.style.display = 'none';
   }
+}
 
-            // except chosen content
+function openTab(tabId) { 
+
+  this.style.flex = 1;
   document.getElementById(tabId).style.display = 'block';
+}
+
+let COUNT = [0,0,0,0];
+for (let i = 0; i < tabLinks.length; i++) {
+  tabLinks[i].addEventListener("click", ()=> {
+    COUNT[i] += 1;
+    console.log(COUNT[i]);
+    console.log(COUNT[i]/2);
+    if (COUNT[i]%2 === 0) {
+      closeTabs();
+    } else {
+      openTab(`tab{tabLinks[i]+1}`)
+    }
+  })
 }
