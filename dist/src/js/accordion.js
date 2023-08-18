30dd3dc57692f3258a2daa7525a80ff1ea7ace1b
Nastia <snasfibz05@gmail.com>
2023-08-18 11:38:14 +0300

    Tabs toggling

--- dist/src/js/accordion.js
+++ dist/src/js/accordion.js
@@ -3,14 +3,13 @@
     for (let i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function(event) {
         let panel = this.nextElementSibling;
-      
+        event.stopPropagation();
                 // toggle between hide / show
         if (panel.style.display === "block") {
           panel.style.display = "none";
         } else {
           panel.style.display = "block";
         }
-        event.stopPropagation();
     
       });
       }
