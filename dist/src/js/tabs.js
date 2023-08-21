05bc62501a6393fb4f89c56672098629afa6a3d1
Nastia <snasfibz05@gmail.com>
2023-08-21 15:32:26 +0300

    Toggle tabs fix

--- dist/src/js/tabs.js
+++ dist/src/js/tabs.js
@@ -6,11 +6,11 @@ const tabContents = Array.from(document.querySelectorAll('.tab-content'));
 for (let i = 0; i < tabLinks.length; i++) {
   tabLinks[i].addEventListener("click", ()=> {
     
-    if (tabLinks[i].style.display.flex == 0) {
-      tabLinks[i].style.flex = 1;
+    if (tabLinks[i].style.display.flex == 1) {
+      tabLinks[i].style.flex = 2;
       tabContents[i].style.display = "block";
     } else {
-      tabLinks[i].style.flex = 0;
+      tabLinks[i].style.flex = 1;
       tabContents[i].style.display = "none";
     }
   })
