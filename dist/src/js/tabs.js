8b9f08ee6ff47f4fecd8b053759ddcc271d395d2
Nastia <snasfibz05@gmail.com>
2023-10-01 14:37:56 +0300

    Mobile bg and Tabs

--- dist/src/js/tabs.js
+++ dist/src/js/tabs.js
@@ -6,19 +6,21 @@ const tabContents = Array.from(document.querySelectorAll('.tab-content'));
 for (let i = 0; i < tabLinks.length; i++) {
   tabLinks[i].addEventListener("click", ()=> {
     
-    if (tabLinks[i].style.flex === 1 || tabLinks[i].style.flex === 0) {
+    if (tabLinks[i].style.flex == '1' || tabLinks[i].style.flex == '0') {
       for (let tablink of tabLinks) {
-        tablink.style.flex = 1;
+        tablink.style.flex = '1';
       }
-      tabLinks[i].style.flex = 2; // selected
+      tabLinks[i].style.flex = '2'; // selected
       tabLinks[i].lastElementChild.style.display = "block";
-    } else if (tabLinks[i].style.flex === 2) {
+    }
+    
+    else if (tabLinks[i].style.flex == '2') {
       console.log('closing!!!');
       for (let k = 0; k < tabLinks.length-1; k++) {
-        tabLinks[k].style.flex = 1;
+        tabLinks[k].style.flex = '1';
         tabLinks[k].lastElementChild.style.display = "none";
       }
-      tabLinks[3].style.flex = 0;
+      tabLinks[3].style.flex = '0';
       tabContents[i].style.display = "none";
     }
   })
