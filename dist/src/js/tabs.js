1e72bb5a874cfa6fdb1073ade3413191562c2dc6
Nastia <snasfibz05@gmail.com>
2023-10-01 17:05:34 +0300

    Mobile bg and Tabs

--- dist/src/js/tabs.js
+++ dist/src/js/tabs.js
@@ -1,25 +1,41 @@
-    // SWITCHING THE TABS
-            // (tab-content, clicked tab-link)
-            function openTab(tabId, elem) { 
-              // tab-link collection
-    let tabLinks = Array.from(document.getElementById('tabs-nav').children);
-  
-              // make all tabs thin
-    for (let tab of tabLinks) {
-      tab.style.flex = 0;
-    }
-  
-              // except the one that was clicked
-    elem.style.flex = 1;
-  
-              // find all content
-    let tabContents = document.querySelectorAll('.tab-content');
-    
-              // hide everything
-    for (let content of tabContents) {
-      content.style.display = 'none';
-    }
-  
-              // except chosen content
-    document.getElementById(tabId).style.display = 'block';
+// SWITCHING THE TABS
+        // (tab-content, clicked tab-link)
+function openTab(tabId, elem) { 
+          // tab-link collection
+  let tabLinks = Array.from(document.getElementById('tabs-nav').children);
+
+          // make all tabs thin
+  for (let tab of tabLinks) {
+    tab.style.flex = 0;
   }
+
+          // except the one that was clicked
+  elem.style.flex = 1;
+  if (window.innerWidth < 1025) {
+    switch (elem) {
+      case tab1:
+        elem.style.background = '#fff';
+        break;
+      case tab2:
+        elem.style.background = '#ff0';
+        break;
+      case tab3:
+        elem.style.background = '#f00';
+        break;
+      case about:
+        elem.style.background = '#000';
+        break;
+    }
+  }
+
+          // find all content
+  let tabContents = document.querySelectorAll('.tab-content');
+
+          // hide everything
+  for (let content of tabContents) {
+    content.style.display = 'none';
+  }
+
+          // except chosen content
+  document.getElementById(tabId).style.display = 'block';
+}
