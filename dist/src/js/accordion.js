45299a280ec8282dfbb9659a76bed9852848e9ec
Nastia <snasfibz05@gmail.com>
2023-12-05 17:04:47 +0100

    Mobile accordion

--- dist/src/js/accordion.js
+++ dist/src/js/accordion.js
@@ -1,9 +1,12 @@
     // OPEN/CLOSE ACCORDION
     let acc = document.getElementsByClassName("accordion");
+    let arrAnim = document.querySelectorAll("arrow-animation");
     for (let i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
         let panel = this.lastElementChild;
-      
+        if (window.matchMedia("(max-width: 1024px)").matches) {
+          arrAnim.style.display = "none";
+        }
                 // toggle between hide / show
         if (panel.style.display === "block") {
           panel.style.display = "none";
