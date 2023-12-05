95e05a902cc32d6ee02223741eb2067b7b94ccb5
Nastia <snasfibz05@gmail.com>
2023-12-05 17:15:04 +0100

    Mobile accordion

--- dist/src/js/accordion.js
+++ dist/src/js/accordion.js
@@ -4,6 +4,16 @@
     for (let i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
         let panel = this.lastElementChild;
+
+                // for mobile
+        if (window.matchMedia("(max-width: 1024px)").matches) {
+          arrAnim.style.display = "none";
+          if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+        }
                 // toggle between hide / show
         if (panel.style.display === "none") {
           panel.style.display = "block";
