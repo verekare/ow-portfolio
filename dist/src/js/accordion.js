f77400508d665c87444a292283e4f800e903941e
Nastia <snasfibz05@gmail.com>
2023-12-05 17:32:50 +0100

    Mobile accordion

--- dist/src/js/accordion.js
+++ dist/src/js/accordion.js
@@ -1,29 +1,29 @@
-    // OPEN/CLOSE ACCORDION
-    let acc = document.getElementsByClassName("accordion");
-    let arrAnim = document.getElementsByClassName("arrow-animation");
-    for (let i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        let panel = this.lastElementChild;
+// OPEN/CLOSE ACCORDION
+let acc = document.getElementsByClassName("accordion");
+let arrAnim = document.getElementsByClassName("arrow-animation");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    let panel = this.lastElementChild;
 
-                // for mobile
-        if (window.matchMedia("(max-width: 1024px)").matches) {
-          if (panel.style.display === "none") {
-            panel.style.display = "block";
-          } else {
-            panel.style.display = "none";
-          }
-        }
-                // toggle between hide / show
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-    
-      });
+            // for mobile
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+      if (panel.style.display === "none") {
+        panel.style.display = "block";
+      } else {
+        panel.style.display = "none";
+      }
+    }
+            // toggle between hide / show
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
 
-      if (window.matchMedia("(max-width: 1024px)").matches) {
-        arrAnim.style.display = "none";
-        console.log(arrAnim);
-      }
+  });
+}
+
+console.log(arrAnim);
+if (window.matchMedia("(max-width: 1024px)").matches) {
+  arrAnim.style.display = "none";
+}
